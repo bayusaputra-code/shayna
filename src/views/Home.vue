0d6ab215147b9513eb1c4df69139609e66c2934f
bayusaputra <bayu24622@gmail.com>
2020-03-12 12:49:31 +0700

    upload projek vue

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+       <HeaderShayna />
+       <HeroShayna />
+       <ListShayna />
+       <InstaShayna />
+       <PartnerShayna />
+       <FooterShayna />
+       </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import ListShayna from "@/components/ListShayna.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PartnerShayna from "@/components/PartnerShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+export default {
+  name: 'Home',
+  components: {
+   HeaderShayna,
+   HeroShayna,
+   ListShayna,
+   InstaShayna,
+   PartnerShayna,
+   FooterShayna 
+  }
+}
+</script>
